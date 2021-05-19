a66ea12b3eb506ad22fde622a1c15637bfb5b038
Nagah Shinawy <E-n.Shinawy@lean.sa>
2021-05-20 00:53:46 +0300

    black

--- data.py
+++ data.py
@@ -3,19 +3,14 @@ created by Nagaj at 19/05/2021
 """
 
 turtles_data = [
-    {"color": "red", "position": {"x": -250, "y": -100}, "shape": "turtle", "moves": 0},
-    {"color": "green", "position": {"x": -250, "y": -80}, "shape": "turtle", "moves": 0},
-    {"color": "blue", "position": {"x": -250, "y": -60}, "shape": "turtle", "moves": 0},
-    {
-        "color": "medium slate blue",
-        "position": {"x": -250, "y": -40},
-        "shape": "turtle",
-        "moves": 0
-    },
-    {"color": "purple", "position": {"x": -250, "y": -20}, "shape": "turtle", "moves": 0},
-    {"color": "chartreuse", "position": {"x": -250, "y": 0}, "shape": "turtle", "moves": 0},
-    {"color": "brown", "position": {"x": -250, "y": 20}, "shape": "turtle", "moves": 0},
-    {"color": "deep pink", "position": {"x": -250, "y": 40}, "shape": "turtle", "moves": 0},
-    {"color": "yellow", "position": {"x": -250, "y": 60}, "shape": "turtle", "moves": 0},
-    {"color": "white", "position": {"x": -250, "y": 80}, "shape": "turtle", "moves": 0},
+    {"color": "red", "position": {"x": -250, "y": -100}, "shape": "turtle"},
+    {"color": "green", "position": {"x": -250, "y": -80}, "shape": "turtle"},
+    {"color": "blue", "position": {"x": -250, "y": -60}, "shape": "turtle"},
+    {"color": "medium slate blue", "position": {"x": -250, "y": -40}, "shape": "turtle"},
+    {"color": "purple", "position": {"x": -250, "y": -20}, "shape": "turtle"},
+    {"color": "chartreuse", "position": {"x": -250, "y": 0}, "shape": "turtle"},
+    {"color": "brown", "position": {"x": -250, "y": 20}, "shape": "turtle"},
+    {"color": "deep pink", "position": {"x": -250, "y": 40}, "shape": "turtle"},
+    {"color": "yellow", "position": {"x": -250, "y": 60}, "shape": "turtle"},
+    {"color": "white", "position": {"x": -250, "y": 80}, "shape": "turtle"},
 ]
